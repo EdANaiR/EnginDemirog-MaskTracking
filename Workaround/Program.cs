8969fb00ec3f4dcea491e6dc217f431be856ee9b
Eda Nur Nair <93126814+EdANaiR@users.noreply.github.com>
2024-02-07 14:27:16 +0300

    C-Sharp Kursu

---
+++ Workaround/Program.cs
@@ -0,0 +1,41 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workaround
+{
+    internal class Program
+    {
+
+        Person person1 = new Person();
+
+            person1.FirstName = "Eda";
+            person1.LastName = "Nair";
+            person1.DateOfBirthYear = 2001;
+            person1.NationalIdentity = 96547854;
+
+            Person person2 = new Person();
+            person2.FirstName = "Hüseyin";
+
+            
+
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+            Console.ReadLine();
+
+
+
+
+            static void Main(string[] args)
+            {
+            Person person = new Person();
+            }
+    }
+}
